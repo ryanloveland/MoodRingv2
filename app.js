343dfc11e060283630b8517722ec15b938398ed9
brandonhawi <brandonhawi@jasonbusiness.com>
2019-10-26 22:09:01 -0700

    change port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 const redirect_uri = 'http://localhost:3000/callback';
 const request = require('request');
 const url = require('url');
